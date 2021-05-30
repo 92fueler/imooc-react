1f9f812064a1b54a561f22fc3e902ed0a0650356
Joyce Wu <jianjoycewu@gmail.com>
2021-05-30 12:53:11 -0700

    rebuilt by react-redux

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,16 +1,53 @@
 import React from 'react';
+import { Input, Button } from 'antd'
+import { connect } from 'react-redux'
 
-export default class TodoList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
+const TodoList = (props) => {
+    const { inputValue, list, changeInputValue, handleClick, deleteItem } = props;
     return (
-      <React.Fragment>
-        
-      </React.Fragment>
+        <>
+            <Input placeholder="Enter your task" value={inputValue} onChange={changeInputValue} />
+            <Button onClick={handleClick}>Submit</Button>
+            <ul>
+                {list.map((item, index) => {
+                    return <li key={`${index}+${item}`} onClick={deleteItem}>{item}</li>
+                })}
+            </ul>
+        </>
     );
-  }
 }
+
+const mapStateToProps = (state) => {
+    return {
+        inputValue: state.inputValue,
+        list: state.list
+    }
+}
+
+// mount store.dispatch on props 
+const mapDispatchToProps = (dispatch) => {
+    return {
+        changeInputValue(e) {
+            const action = {
+                type: 'change_input_value',
+                value: e.target.value,
+            }
+            dispatch(action)
+        },
+        handleClick(e) {
+            const action = {
+                type: 'add_item'
+            }
+            dispatch(action)
+        },
+        deleteItem(index) {
+            const action = {
+                type: 'delete_item',
+                index
+            }
+            dispatch(action)
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
